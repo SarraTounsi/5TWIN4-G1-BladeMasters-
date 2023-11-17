800d80aa6bdb2bc42ea4809ee229118ff0651f42
SarraTounsi <sarra.tounsi@esprit.tn>
2023-11-17 01:15:03 +0100

    tests fixed

--- src/test/java/tn/esprit/spring/kaddem/UnitTest/services/DetailEquipeServiceImplTest.java
+++ src/test/java/tn/esprit/spring/kaddem/UnitTest/services/DetailEquipeServiceImplTest.java
@@ -13,11 +13,14 @@ import org.mockito.MockitoAnnotations;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.mockito.Mockito;
-import tn.esprit.spring.kaddem.entities.DetailEquipe; // Import DetailEquipe class
-import tn.esprit.spring.kaddem.repositories.DetailEquipeRepository; // Import DetailEquipeRepository
-import tn.esprit.spring.kaddem.services.DetailEquipeServiceImpl; // Import DetailEquipeServiceImpl
+import tn.esprit.spring.kaddem.entities.DetailEquipe;
+import tn.esprit.spring.kaddem.repositories.DetailEquipeRepository;
+import tn.esprit.spring.kaddem.services.DetailEquipeServiceImpl;
+
+import javax.persistence.EntityNotFoundException;
 
 public class DetailEquipeServiceImplTest {
 
@@ -25,7 +28,7 @@ public class DetailEquipeServiceImplTest {
     private DetailEquipeServiceImpl detailEquipeService;
 
     @Mock
-    private DetailEquipeRepository detailEquipeRepository; // Change the repository to DetailEquipeRepository
+    private DetailEquipeRepository detailEquipeRepository;
 
     @BeforeEach
     void setUp() {
@@ -70,4 +73,33 @@ public class DetailEquipeServiceImplTest {
         verify(detailEquipeRepository, times(1)).findById(detailEquipeIdToRetrieve);
         assertEquals(expectedDetailEquipe, result);
     }
+    @Test
+    public void deleteDetailEquipeTest() {
+        Integer detailEquipeIdToDelete = 1;
+        doNothing().when(detailEquipeRepository).deleteById(detailEquipeIdToDelete);
+        detailEquipeService.removeDetailEquipe(detailEquipeIdToDelete);
+        verify(detailEquipeRepository, times(1)).deleteById(detailEquipeIdToDelete);
+    }
+
+    @Test
+    public void updateDetailEquipeTest() {
+        Integer detailEquipeIdToUpdate = 1;
+        DetailEquipe existingDetailEquipe = new DetailEquipe(detailEquipeIdToUpdate, 123, "Sample Thematique");
+        DetailEquipe updatedDetailEquipe = new DetailEquipe(detailEquipeIdToUpdate, 456, "Updated Thematique");
+
+        when(detailEquipeRepository.findById(detailEquipeIdToUpdate)).thenReturn(Optional.of(existingDetailEquipe));
+        when(detailEquipeRepository.save(updatedDetailEquipe)).thenReturn(updatedDetailEquipe);
+
+        DetailEquipe result = detailEquipeService.updateDetailEquipe(updatedDetailEquipe);
+
+        assertThat(result).isNotNull();
+        assertThat(result.getIdDetailEquipe()).isEqualTo(detailEquipeIdToUpdate);
+        assertThat(result.getSalle()).isEqualTo(456);
+        assertThat(result.getThematique()).isEqualTo("Updated Thematique");
+
+        verify(detailEquipeRepository, times(1)).findById(detailEquipeIdToUpdate);
+        verify(detailEquipeRepository, times(1)).save(updatedDetailEquipe);
+    }
+
+
 }
